b0ddc0ba2a0eaf5cd865fb36d290a2881bf229ac
quanlj <qljqlj44@sina.com>
2019-03-09 21:43:14 +0800

    提交代码

---
+++ 01code/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+	@Autowired
+    private TestRestTemplate restTemplate;
+    @Test
+    public void testScore(){
+        String jsonStr = "{\"data\":{\"debit_account_balance_code\":40,\"credit_consume_count\":1,\"debit_start_age\":1,\"debit_consume_sum_code\":2,\"age\":38},\"modelProductId\":55}";
+        String content = this.restTemplate.getForObject("/h", String.class);
+        System.out.println(content);
+    }
+
+}
